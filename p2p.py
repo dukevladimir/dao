a4616ae45723b7829f5571b24b8f31901a203686
Andy <andymarsden100@googlemail.com>
2018-03-22 08:38:03 +0000

    p2p updates

--- p2p.py
+++ p2p.py
@@ -1,6 +1,25 @@
+import os
+import sys
+
+NAME = "Dune"
+VERSION = "0.01"
+CONFIG_FILE = "./config-file.json"
+NODE_LIST = "./node-list.json"
+
+class node:
+
+	def __init__(config = None, nodes = None):
+		
+		if config is not None:
+			self._config = config
+		else:
+			# create config file 
+
 
 def main():
-	pass
+	print(">> starting {} p2p node - version {}".format(NAME,VERSION))
+	print(">> searching for config-file in location '{}'".format(CONFIG_FILE))
+
 
 if __name__ == '__main__':
 	main()
